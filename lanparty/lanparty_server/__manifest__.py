a4ecd59cfe22d3852b184254adb2bf8fda0f8f78
Luca Cireddu <sardylan@gmail.com>
2018-12-16 15:28:58 +0100

    Adds lanparty_server addon

---
+++ lanparty/lanparty_server/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "LAN Party Server",
+    "version": "12.0.0.0.0",
+    "summary": "LAN Party server for tools",
+    "description": """
+LAN Party Server
+====================
+LAN Party Server for tool API and management
+    """,
+    "category": "Other Tools",
+    "website": "https://www.thehellnet.org",
+    "depends": [
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False
+}
